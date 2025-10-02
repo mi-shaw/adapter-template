9543b783fd26899dd7194ca786faa5e43e81e5bb
Michael Shaw <michaelshaw@Michaels-MacBook-Air.local>
2025-10-01 19:43:02 -0700

    Change name of package to mi-shaw/adapter-template

--- cmd/adapter/main.go
+++ cmd/adapter/main.go
@@ -21,9 +21,9 @@ import (
 	"net"
 	"os"
 
+	"github.com/mi-shaw/adapter-template/pkg/adapter"
 	api_adapter_v1 "github.com/sgnl-ai/adapter-framework/api/adapter/v1"
 	"github.com/sgnl-ai/adapter-framework/server"
-	"github.com/sgnl-ai/adapter-template/pkg/adapter"
 	"google.golang.org/grpc"
 )
 
--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module github.com/sgnl-ai/adapter-template
+module github.com/mi-shaw/adapter-template
 
 go 1.21
 
